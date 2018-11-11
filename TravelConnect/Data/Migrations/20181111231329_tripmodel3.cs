fc164ffc55180da890084802e909cdacafe9ef33
Jason Jacobs <jacobs.jb@gmail.com>
2018-11-11 17:14:01 -0600

    migration

---
+++ TravelConnect/Data/Migrations/20181111231329_tripmodel3.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelConnect.Data.Migrations
+{
+    public partial class tripmodel3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TripDescription",
+                table: "TripModel",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DestinationCity",
+                table: "TripModel",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DepartureCity",
+                table: "TripModel",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TripDescription",
+                table: "TripModel",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DestinationCity",
+                table: "TripModel",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DepartureCity",
+                table: "TripModel",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
